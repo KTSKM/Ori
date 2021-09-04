8ccc88270ab38216b902b002084eec9db2f17aae
macro-butdiffrent <85488903+macro-butdiffrent@users.noreply.github.com>
2021-09-04 17:59:30 +0800

    Update ORiON.py

--- cogs/ORiON.py
+++ cogs/ORiON.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 
 class ORiON(commands.Cog, name='ORiON'):
     def __init__(self, bot):
@@ -7,7 +7,7 @@ class ORiON(commands.Cog, name='ORiON'):
     
     @commands.command(description= "Tells you how to join ORiON!")
     async def join(self, ctx):
-        embed=discord.Embed(title="read the pinned message at announcements u dumdum", url="https://discord.com/channels/880016735980363776/880036716377948190/880497534357864558", description="u didn't read the rules didn't u")
+        embed=nextcord.Embed(title="read the pinned message at announcements u dumdum", url="https://discord.com/channels/880016735980363776/880036716377948190/880497534357864558", description="u didn't read the rules didn't u")
         await ctx.reply(embed=embed)
 
 def setup(bot):
