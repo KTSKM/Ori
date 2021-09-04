f77e3e9809f346c0ad8c6e2b8678b1e2871391a1
macro-butdiffrent <85488903+macro-butdiffrent@users.noreply.github.com>
2021-09-04 18:08:11 +0800

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -17,7 +17,7 @@ class moderation(commands.Cog, name='moderation'):
     ## KICK
     @commands.command(aliases=['k'], description= 'kicks specified member. only people with "kick_members" perm can use this. ')
     @commands.has_guild_permissions(kick_members=True)
-    async def kick(self,ctx, member : discord.Member,*,reason= "No reason provided"):
+    async def kick(self,ctx, member : nextcord.Member,*,reason= "No reason provided"):
         try:
             await member.send("You've been kicked from ORiON, my guess: you were naughty. Oh! Here's the real reason: "+reason)
         except:
@@ -28,7 +28,7 @@ class moderation(commands.Cog, name='moderation'):
     ## BAN
     @commands.command(aliases=['b'], description= 'bans specified member. only people with "ban_members" perm can use this. ')
     @commands.has_guild_permissions(ban_members=True)
-    async def ban(self,ctx, member : discord.Member,*,reason= "No reason provided"):
+    async def ban(self,ctx, member : nextcord.Member,*,reason= "No reason provided"):
 
         await member.send("You've been banned from ORiON, my guess: you were *real* naughty. Oh! Here's the real reason: "+reason)
         await member.ban(reason=reason)
@@ -54,7 +54,7 @@ class moderation(commands.Cog, name='moderation'):
     ## MUTE
     @commands.command(aliases=['m'], description= 'mutes specified member. only people with "kick_members" perm can use this. ')
     @commands.has_guild_permissions(kick_members=True)
-    async def mute(self,ctx, member : discord.Member,*,reason="No reason provided"):
+    async def mute(self,ctx, member : nextcord.Member,*,reason="No reason provided"):
         muted_role = ctx.guild.get_role(880016735980363785)
 
         await member.add_roles(muted_role)
@@ -64,7 +64,7 @@ class moderation(commands.Cog, name='moderation'):
     ## UNMUTE
     @commands.command(aliases=['um'], description= 'unmutes specified member. only people with "kick_members" perm can use this. ')
     @commands.has_guild_permissions(kick_members=True)
-    async def unmute(self,ctx,member : discord.Member,*,reason= "No reason provided"):
+    async def unmute(self,ctx,member : nextcord.Member,*,reason= "No reason provided"):
         muted_role = ctx.guild.get_role(880016735980363785)
 
         await member.remove_roles(muted_role)
@@ -74,7 +74,7 @@ class moderation(commands.Cog, name='moderation'):
     ## WHOIS
     @commands.command(aliases=['info', 'user', 'userinfo'], description= 'tells you the information of the user mentioned.')
     @commands.has_guild_permissions(kick_members=True)
-    async def whois(self, ctx, member : discord.Member):
+    async def whois(self, ctx, member : nextcord.Member):
         created_at = member.created_at.strftime("%b %d, %Y")
         roles = [role for role in member.roles]
 
